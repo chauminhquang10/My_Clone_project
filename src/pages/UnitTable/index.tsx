11737b45043e58a670eca1c49e8ce4e02393a77b
phuccao.30012001@gmail.com <phuccao.30012001@gmail.com>
2022-09-15 15:40:17 +0700

    fix: table user management

--- src/pages/UnitTable/index.tsx
+++ src/pages/UnitTable/index.tsx
@@ -1,16 +1,47 @@
-import { addRule, unit } from "@/services/ant-design-pro/api";
-import type { ActionType, ProColumns } from "@ant-design/pro-components";
-import { PageContainer, ProTable } from "@ant-design/pro-components";
-import UnitDetailDrawer from "./components/forms/UnitDetailDrawer";
-import { message } from "antd";
-import { useRef, useState } from "react";
-import AddNew from "@/components/TableProperties/AddNew";
-import Column from "./components/tables/Column";
+import { addRule } from '@/services/ant-design-pro/api';
+import type { ActionType, ProColumns } from '@ant-design/pro-components';
+import { PageContainer, ProTable } from '@ant-design/pro-components';
+import UnitDetailDrawer from './components/forms/UnitDetailDrawer';
+import { message } from 'antd';
+import { useRef, useState } from 'react';
+import AddNew from '@/components/TableProperties/AddNew';
+import Column from './components/tables/Column';
 // import SelectPage from "./components/tables/SelectPage";
-import style from "@/components/TableProperties/style.less";
-import TitleTable from "@/components/TableProperties/TitleTable";
-import TotalPagination from "@/components/TableProperties/TotalPagination";
-import NewUnitForm from "./components/forms/NewUnitForm";
+import style from '@/components/TableProperties/style.less';
+import TitleTable from '@/components/TableProperties/TitleTable';
+import TotalPagination from '@/components/TableProperties/TotalPagination';
+import NewUnitForm from './components/forms/NewUnitForm';
+
+const genListUnit = (current: number, pageSize: number) => {
+  const tableListDataSource: API.ManagementUnitResponse[] = [];
+
+  for (let i = 0; i < pageSize; i += 1) {
+    const index = (current - 1) * 10 + i;
+    tableListDataSource.push({
+      id: index,
+      code: `code-${index}`,
+      name: `name-${index}`,
+      location: `location-${index}`,
+      province: {
+        id: 1,
+        name: `province-${index}`,
+      },
+      district: {
+        id: 1,
+        name: `districtrict-${index}`,
+      },
+      ward: {
+        id: 1,
+        name: `ward-${index}`,
+      },
+      address: `address-${index}`,
+      createdAt: `createdAt-${index}`,
+    });
+  }
+  return tableListDataSource;
+};
+
+const listUnit = genListUnit(1, 100);
 
 /**
  * @en-US Add node
@@ -18,154 +49,150 @@ import NewUnitForm from "./components/forms/NewUnitForm";
  * @param fields
  */
 const handleAdd = async (fields: API.ManagementUnitResponse) => {
-    const hide = message.loading("正在添加");
-    try {
-        await addRule({ ...fields });
-        hide();
-        message.success("Added successfully");
-        return true;
-    } catch (error) {
-        hide();
-        message.error("Adding failed, please try again!");
-        return false;
-    }
+  const hide = message.loading('正在添加');
+  try {
+    await addRule({ ...fields });
+    hide();
+    message.success('Added successfully');
+    return true;
+  } catch (error) {
+    hide();
+    message.error('Adding failed, please try again!');
+    return false;
+  }
 };
 
 const TableCustom = () => {
-    /**
-     * @en-US Pop-up window of new window
-     * @zh-CN 新建窗口的弹窗
-     *  */
-    const [createModalVisible, handleCreateModalVisible] =
-        useState<boolean>(false);
-    /**
-     * @en-US The pop-up window of the distribution update window
-     * @zh-CN 分布更新窗口的弹窗
-     * */
-    // const [updateModalVisible, handleUpdateModalVisible] = useState<boolean>(false);
+  /**
+   * @en-US Pop-up window of new window
+   * @zh-CN 新建窗口的弹窗
+   *  */
+  const [createModalVisible, handleCreateModalVisible] = useState<boolean>(false);
+  /**
+   * @en-US The pop-up window of the distribution update window
+   * @zh-CN 分布更新窗口的弹窗
+   * */
+  // const [updateModalVisible, handleUpdateModalVisible] = useState<boolean>(false);
 
-    const [showDetail, setShowDetail] = useState<boolean>(false);
+  const [showDetail, setShowDetail] = useState<boolean>(false);
 
-    const actionRef = useRef<ActionType>();
-    const [currentRow, setCurrentRow] = useState<API.ManagementUnitResponse>();
+  const actionRef = useRef<ActionType>();
+  const [currentRow, setCurrentRow] = useState<API.ManagementUnitResponse>();
 
-    /**
-     * @en-US International configuration
-     * @zh-CN 国际化配置
-     * */
+  /**
+   * @en-US International configuration
+   * @zh-CN 国际化配置
+   * */
 
-    // const [page, setPage] = useState<number>();
-    // const [pageSize, setPageSize] = useState<number>();
-    // const pageSizeRef = useRef<number>(20);
-    const columns: ProColumns<API.ManagementUnitResponse>[] = Column({
-        setCurrentRow,
-        setShowDetail,
-    });
+  // const [page, setPage] = useState<number>();
+  // const [pageSize, setPageSize] = useState<number>();
+  // const pageSizeRef = useRef<number>(20);
+  const columns: ProColumns<API.ManagementUnitResponse>[] = Column({
+    setCurrentRow,
+    setShowDetail,
+  });
 
-    const [currentPage, setCurrentPage] = useState<number>(0);
-    const pageSize = useRef<number>(20);
-    // const [totalPage, setTotalPage] = useState<number>(1);
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  const pageSize = useRef<number>(20);
+  // const [totalPage, setTotalPage] = useState<number>(1);
 
-    //-------------- Pagination props --------------------------------
-    const paginationLocale = {
-        items_per_page: "",
-        jump_to: "Trang",
-        page: "",
-    };
-    return (
-        <PageContainer
-            className={style["table-container"]}
-            header={{
-                title: "",
+  //-------------- Pagination props --------------------------------
+  const paginationLocale = {
+    items_per_page: '',
+    jump_to: 'Trang',
+    page: '',
+  };
+  return (
+    <PageContainer
+      className={style['table-container']}
+      header={{
+        title: '',
+      }}
+      footer={undefined}
+    >
+      <ProTable
+        headerTitle={<TitleTable>Đơn vị quản lý</TitleTable>}
+        actionRef={actionRef}
+        rowKey="key"
+        search={false}
+        toolBarRender={() => [
+          <AddNew
+            key="primary"
+            onClick={() => {
+              handleCreateModalVisible(true);
             }}
-            footer={undefined}
-        >
-            <ProTable
-                headerTitle={<TitleTable>Đơn vị quản lý</TitleTable>}
-                actionRef={actionRef}
-                rowKey="key"
-                search={false}
-                toolBarRender={() => [
-                    <AddNew
-                        key="primary"
-                        onClick={() => {
-                            handleCreateModalVisible(true);
-                        }}
-                    />,
-                ]}
-                request={unit}
-                // request={async (params = {}) => {
-                //     const filterParams: API.UserFilter = {
-                //         managementUnit: "",
-                //         staffId: "",
-                //     };
+          />,
+        ]}
+        // request={unit}
+        dataSource={listUnit}
+        // request={async (params = {}) => {
+        //     const filterParams: API.UserFilter = {
+        //         managementUnit: "",
+        //         staffId: "",
+        //     };
 
-                //     const pageRequestParams: API.PageReq = {
-                //         pageNumber: params.current,
-                //         pageSize: params.pageSize,
-                //         sortDirection: "",
-                //         sortBy: "",
-                //     };
-                //     await runGetAllUser({
-                //         filter: filterParams,
-                //         pageRequest: pageRequestParams,
-                //     });
-                //     return {
-                //         data: listUser,
-                //     };
-                // }}
-                columns={columns}
-                options={false}
-                // rowSelection={{
-                //     onChange: (_, selectedRows) => {
-                //         setSelectedRows(selectedRows);
-                //     },
-                // }}
-                pagination={{
-                    onChange(current) {
-                        setCurrentPage(current);
-                    },
-                    current: currentPage,
-                    className: style["pagination-custom"],
-                    locale: { ...paginationLocale },
-                    showSizeChanger: false,
-                    pageSize: pageSize.current,
-                    showTotal: (total, range) => (
-                        <TotalPagination total={total} range={range} />
-                    ),
-                    hideOnSinglePage: true,
-                    showQuickJumper: true,
-                }}
-            />
+        //     const pageRequestParams: API.PageReq = {
+        //         pageNumber: params.current,
+        //         pageSize: params.pageSize,
+        //         sortDirection: "",
+        //         sortBy: "",
+        //     };
+        //     await runGetAllUser({
+        //         filter: filterParams,
+        //         pageRequest: pageRequestParams,
+        //     });
+        //     return {
+        //         data: listUser,
+        //     };
+        // }}
+        columns={columns}
+        options={false}
+        // rowSelection={{
+        //     onChange: (_, selectedRows) => {
+        //         setSelectedRows(selectedRows);
+        //     },
+        // }}
+        pagination={{
+          onChange(current) {
+            setCurrentPage(current);
+          },
+          current: currentPage,
+          className: style['pagination-custom'],
+          locale: { ...paginationLocale },
+          showSizeChanger: false,
+          pageSize: pageSize.current,
+          showTotal: (total, range) => <TotalPagination total={total} range={range} />,
+          hideOnSinglePage: true,
+          showQuickJumper: true,
+        }}
+      />
 
-            <NewUnitForm
-                title="Tạo đơn vị quản lý mới"
-                width="934px"
-                visible={createModalVisible}
-                onVisibleChange={handleCreateModalVisible}
-                onFinish={async (value) => {
-                    const success = await handleAdd(
-                        value as API.ManagementUnitResponse
-                    );
-                    if (success) {
-                        handleCreateModalVisible(false);
-                        if (actionRef.current) {
-                            actionRef.current.reload();
-                        }
-                        return true;
-                    }
-                    return false;
-                }}
-            />
+      <NewUnitForm
+        title="Tạo đơn vị quản lý mới"
+        width="934px"
+        visible={createModalVisible}
+        onVisibleChange={handleCreateModalVisible}
+        onFinish={async (value) => {
+          const success = await handleAdd(value as API.ManagementUnitResponse);
+          if (success) {
+            handleCreateModalVisible(false);
+            if (actionRef.current) {
+              actionRef.current.reload();
+            }
+            return true;
+          }
+          return false;
+        }}
+      />
 
-            <UnitDetailDrawer
-                currentRow={currentRow}
-                setCurrentRow={setCurrentRow}
-                showDetail={showDetail}
-                setShowDetail={setShowDetail}
-            />
-        </PageContainer>
-    );
+      <UnitDetailDrawer
+        currentRow={currentRow}
+        setCurrentRow={setCurrentRow}
+        showDetail={showDetail}
+        setShowDetail={setShowDetail}
+      />
+    </PageContainer>
+  );
 };
 
 export default TableCustom;
--- src/pages/UserTable/index.tsx
+++ src/pages/UserTable/index.tsx
@@ -1,24 +1,42 @@
-import { addRule, rule } from "@/services/ant-design-pro/api";
-import type { ActionType, ProColumns } from "@ant-design/pro-components";
+import { addRule } from '@/services/ant-design-pro/api';
+import type { ActionType, ProColumns } from '@ant-design/pro-components';
 // import { getAllUsers } from "@/services/STM-APIs/UserController";
-import {
-    PageContainer,
-    ProFormText,
-    ProFormTextArea,
-    ProTable,
-} from "@ant-design/pro-components";
-import { message } from "antd";
-import { useRef, useState } from "react";
-import { FormattedMessage } from "umi";
+import { PageContainer, ProFormText, ProFormTextArea, ProTable } from '@ant-design/pro-components';
+import { message } from 'antd';
+import { useRef, useState } from 'react';
+import { FormattedMessage } from 'umi';
 // import {useRequest} from "umi";
-import NewUserForm from "./components/forms/NewUserForm";
-import UserDetailDrawer from "./components/forms/UserDetailDrawer";
-import AddNew from "@/components/TableProperties/AddNew";
-import Column from "./components/tables/Column";
+import NewUserForm from './components/forms/NewUserForm';
+import UserDetailDrawer from './components/forms/UserDetailDrawer';
+import AddNew from '@/components/TableProperties/AddNew';
+import Column from './components/tables/Column';
 // import SelectPage from "./components/tables/SelectPage";
-import style from "@/components/TableProperties/style.less";
-import TitleTable from "@/components/TableProperties/TitleTable";
-import TotalPagination from "@/components/TableProperties/TotalPagination";
+import style from '@/components/TableProperties/style.less';
+import TitleTable from '@/components/TableProperties/TitleTable';
+import TotalPagination from '@/components/TableProperties/TotalPagination';
+
+const genList = (current: number, pageSize: number) => {
+  const tableListDataSource: API.UserResponse[] = [];
+
+  for (let i = 0; i < pageSize; i += 1) {
+    const index = (current - 1) * 10 + i;
+    tableListDataSource.push({
+      id: `${index}`,
+      name: `TradeCode-${index}`,
+      staffId: `No-${index}`,
+      email: `Email${index}@gmail.com`,
+      phoneNumber: `${Math.floor(Math.random() * 1000)}`,
+      status: 'ACTIVE',
+      managementUnit: {
+        code: 'ABC',
+        name: `${index}`,
+      },
+    });
+  }
+  return tableListDataSource;
+};
+
+const tableListDataSource = genList(1, 100);
 
 /**
  * @en-US Add node
@@ -26,187 +44,186 @@ import TotalPagination from "@/components/TableProperties/TotalPagination";
  * @param fields
  */
 const handleAdd = async (fields: API.UserResponse) => {
-    const hide = message.loading("正在添加");
-    try {
-        await addRule({ ...fields });
-        hide();
-        message.success("Added successfully");
-        return true;
-    } catch (error) {
-        hide();
-        message.error("Adding failed, please try again!");
-        return false;
-    }
+  const hide = message.loading('正在添加');
+  try {
+    await addRule({ ...fields });
+    hide();
+    message.success('Added successfully');
+    return true;
+  } catch (error) {
+    hide();
+    message.error('Adding failed, please try again!');
+    return false;
+  }
 };
 
 const TableCustom = () => {
-    //--------------- listUSer -----------------------------------
-    // const [listUser, setListUser] = useState<API.UserResponse[] | undefined>();
-    //---------------  handle getAllUser -------------------------------
+  //--------------- listUSer -----------------------------------
+  // const [listUser, setListUser] = useState<API.UserResponse[] | undefined>();
+  //---------------  handle getAllUser -------------------------------
 
-    // const { run: runGetAllUser } = useRequest(
-    //     (params: API.getAllUsersParams) => getAllUsers(params),
-    //     {
-    //         manual: true,
-    //         onSuccess: (res) => {
-    //             const data = res as API.ResponseBasePageResponseObject;
-    //             const listUserRespone = data.data?.items;
-    //             setListUser(listUserRespone);
-    //         },
-    //         onError: (error) => {
-    //             console.log(error);
-    //         },
-    //     }
-    // );
-    /**
-     * @en-US Pop-up window of new window
-     * @zh-CN 新建窗口的弹窗
-     *  */
-    const [createModalVisible, handleModalVisible] = useState<boolean>(false);
-    /**
-     * @en-US The pop-up window of the distribution update window
-     * @zh-CN 分布更新窗口的弹窗
-     * */
-    // const [updateModalVisible, handleUpdateModalVisible] = useState<boolean>(false);
+  // const { run: runGetAllUser } = useRequest(
+  //     (params: API.getAllUsersParams) => getAllUsers(params),
+  //     {
+  //         manual: true,
+  //         onSuccess: (res) => {
+  //             const data = res as API.ResponseBasePageResponseObject;
+  //             const listUserRespone = data.data?.items;
+  //             setListUser(listUserRespone);
+  //         },
+  //         onError: (error) => {
+  //             console.log(error);
+  //         },
+  //     }
+  // );
+  /**
+   * @en-US Pop-up window of new window
+   * @zh-CN 新建窗口的弹窗
+   *  */
+  const [createModalVisible, handleModalVisible] = useState<boolean>(false);
+  /**
+   * @en-US The pop-up window of the distribution update window
+   * @zh-CN 分布更新窗口的弹窗
+   * */
+  // const [updateModalVisible, handleUpdateModalVisible] = useState<boolean>(false);
 
-    const [showDetail, setShowDetail] = useState<boolean>(false);
+  const [showDetail, setShowDetail] = useState<boolean>(false);
 
-    const actionRef = useRef<ActionType>();
-    const [currentRow, setCurrentRow] = useState<API.UserResponse>();
+  const actionRef = useRef<ActionType>();
+  const [currentRow, setCurrentRow] = useState<API.UserResponse>();
 
-    /**
-     * @en-US International configuration
-     * @zh-CN 国际化配置
-     * */
+  /**
+   * @en-US International configuration
+   * @zh-CN 国际化配置
+   * */
 
-    // const [page, setPage] = useState<number>();
-    // const [pageSize, setPageSize] = useState<number>();
-    // const pageSizeRef = useRef<number>(20);
-    const columns: ProColumns<API.UserResponse>[] = Column({
-        setCurrentRow,
-        setShowDetail,
-    });
+  // const [page, setPage] = useState<number>();
+  // const [pageSize, setPageSize] = useState<number>();
+  // const pageSizeRef = useRef<number>(20);
+  const columns: ProColumns<API.UserResponse>[] = Column({
+    setCurrentRow,
+    setShowDetail,
+  });
 
-    const [currentPage, setCurrentPage] = useState<number>(0);
-    const pageSize = useRef<number>(20);
-    // const [totalPage, setTotalPage] = useState<number>(1);
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  const pageSize = useRef<number>(20);
+  // const [totalPage, setTotalPage] = useState<number>(1);
 
-    //-------------- Pagination props --------------------------------
-    const paginationLocale = {
-        items_per_page: "",
-        jump_to: "Trang",
-        page: "",
-    };
+  //-------------- Pagination props --------------------------------
+  const paginationLocale = {
+    items_per_page: '',
+    jump_to: 'Trang',
+    page: '',
+  };
 
-    return (
-        <PageContainer
-            className={style["table-container"]}
-            header={{
-                title: "",
+  return (
+    <PageContainer
+      className={style['table-container']}
+      header={{
+        title: '',
+      }}
+      footer={undefined}
+    >
+      <ProTable
+        headerTitle={<TitleTable>Danh sách người dùng</TitleTable>}
+        actionRef={actionRef}
+        rowKey="key"
+        search={false}
+        toolBarRender={() => [
+          <AddNew
+            key="primary"
+            onClick={() => {
+              handleModalVisible(true);
             }}
-            footer={undefined}
-        >
-            <ProTable
-                headerTitle={<TitleTable>Danh sách người dùng</TitleTable>}
-                actionRef={actionRef}
-                rowKey="key"
-                search={false}
-                toolBarRender={() => [
-                    <AddNew
-                        key="primary"
-                        onClick={() => {
-                            handleModalVisible(true);
-                        }}
-                    />,
-                ]}
-                request={rule}
-                // request={async (params = {}) => {
-                //     const filterParams: API.UserFilter = {
-                //         managementUnit: "",
-                //         staffId: "",
-                //     };
+          />,
+        ]}
+        // request={rule}
+        dataSource={tableListDataSource}
+        // request={async (params = {}) => {
+        //     const filterParams: API.UserFilter = {
+        //         managementUnit: "",
+        //         staffId: "",
+        //     };
 
-                //     const pageRequestParams: API.PageReq = {
-                //         pageNumber: params.current,
-                //         pageSize: params.pageSize,
-                //         sortDirection: "",
-                //         sortBy: "",
-                //     };
-                //     await runGetAllUser({
-                //         filter: filterParams,
-                //         pageRequest: pageRequestParams,
-                //     });
-                //     return {
-                //         data: listUser,
-                //     };
-                // }}
-                columns={columns}
-                options={false}
-                // rowSelection={{
-                //     onChange: (_, selectedRows) => {
-                //         setSelectedRows(selectedRows);
-                //     },
-                // }}
-                pagination={{
-                    onChange(current) {
-                        setCurrentPage(current);
-                    },
-                    current: currentPage,
-                    className: style["pagination-custom"],
-                    locale: { ...paginationLocale },
-                    showSizeChanger: false,
-                    pageSize: pageSize.current,
-                    showTotal: (total, range) => (
-                        <TotalPagination total={total} range={range} />
-                    ),
-                    hideOnSinglePage: true,
-                    showQuickJumper: true,
-                }}
-            />
+        //     const pageRequestParams: API.PageReq = {
+        //         pageNumber: params.current,
+        //         pageSize: params.pageSize,
+        //         sortDirection: "",
+        //         sortBy: "",
+        //     };
+        //     await runGetAllUser({
+        //         filter: filterParams,
+        //         pageRequest: pageRequestParams,
+        //     });
+        //     return {
+        //         data: listUser,
+        //     };
+        // }}
+        columns={columns}
+        options={false}
+        // rowSelection={{
+        //     onChange: (_, selectedRows) => {
+        //         setSelectedRows(selectedRows);
+        //     },
+        // }}
+        pagination={{
+          onChange(current) {
+            setCurrentPage(current);
+          },
+          current: currentPage,
+          className: style['pagination-custom'],
+          locale: { ...paginationLocale },
+          showSizeChanger: false,
+          pageSize: pageSize.current,
+          showTotal: (total, range) => <TotalPagination total={total} range={range} />,
+          hideOnSinglePage: true,
+          showQuickJumper: true,
+        }}
+      />
 
-            <NewUserForm
-                title="Tạo người dùng mới"
-                width="934px"
-                visible={createModalVisible}
-                onVisibleChange={handleModalVisible}
-                onFinish={async (value) => {
-                    const success = await handleAdd(value as API.UserResponse);
-                    if (success) {
-                        handleModalVisible(false);
-                        if (actionRef.current) {
-                            actionRef.current.reload();
-                        }
-                        return true;
-                    }
-                    return false;
-                }}
-            >
-                <ProFormText
-                    rules={[
-                        {
-                            required: true,
-                            message: (
-                                <FormattedMessage
-                                    id="pages.searchTable.ruleName"
-                                    defaultMessage="Rule name is required"
-                                />
-                            ),
-                        },
-                    ]}
-                    width="md"
-                    name="name"
+      <NewUserForm
+        title="Tạo người dùng mới"
+        width="934px"
+        visible={createModalVisible}
+        onVisibleChange={handleModalVisible}
+        onFinish={async (value) => {
+          const success = await handleAdd(value as API.UserResponse);
+          if (success) {
+            handleModalVisible(false);
+            if (actionRef.current) {
+              actionRef.current.reload();
+            }
+            return true;
+          }
+          return false;
+        }}
+      >
+        <ProFormText
+          rules={[
+            {
+              required: true,
+              message: (
+                <FormattedMessage
+                  id="pages.searchTable.ruleName"
+                  defaultMessage="Rule name is required"
                 />
-                <ProFormTextArea width="md" name="desc" />
-            </NewUserForm>
-            <UserDetailDrawer
-                currentRow={currentRow}
-                setCurrentRow={setCurrentRow}
-                showDetail={showDetail}
-                setShowDetail={setShowDetail}
-                userAvatar=""
-            />
-        </PageContainer>
-    );
+              ),
+            },
+          ]}
+          width="md"
+          name="name"
+        />
+        <ProFormTextArea width="md" name="desc" />
+      </NewUserForm>
+      <UserDetailDrawer
+        currentRow={currentRow}
+        setCurrentRow={setCurrentRow}
+        showDetail={showDetail}
+        setShowDetail={setShowDetail}
+        userAvatar=""
+      />
+    </PageContainer>
+  );
 };
 
 export default TableCustom;
