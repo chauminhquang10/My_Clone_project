101afc04ae333ca5f7d0061ee241e332dcae2f11
phuccao.30012001@gmail.com <phuccao.30012001@gmail.com>
2022-09-16 16:26:07 +0700

    feat: filter calendar

--- src/pages/StatisActivityTable/components/tables/Column.tsx
+++ src/pages/StatisActivityTable/components/tables/Column.tsx
@@ -1,74 +1,78 @@
-import type { ProColumns } from "@ant-design/pro-components";
-import HeadCell from "@/components/TableProperties/HeadCell";
-import { TextCell } from "@/components/TableProperties//TableCell";
+import type { ProColumns } from '@ant-design/pro-components';
+import HeadCell from '@/components/TableProperties/HeadCell';
+import { TextCell } from '@/components/TableProperties//TableCell';
 
 type ColumnProps = {
-    setCurrentRow: (s: API.StmInfoResponse) => void;
-    setShowDetail: (s: boolean) => void;
+  setCurrentRow: (s: API.StmInfoResponse) => void;
+  setShowDetail: (s: boolean) => void;
+};
+
+type TransactionConfiguration = {
+  machine: API.StmInfoResponse;
+  total: number;
+  success: number;
+  failure: number;
 };
 
 function Column({}: ColumnProps) {
-    const columns: ProColumns<API.StmInfoResponse>[] = [
-        {
-            title: <HeadCell>Tên máy</HeadCell>,
-            dataIndex: "name",
-            render: (dom) => {
-                return <TextCell>{dom}</TextCell>;
-            },
-            sorter: (a, b) => {
-                if (a.name && b.name) return a.name.localeCompare(b.name);
-                else return 1;
-            },
-        },
-        {
-            title: <HeadCell>Terminal ID</HeadCell>,
-            dataIndex: "location",
-            render: (dom) => {
-                return <TextCell>{dom}</TextCell>;
-            },
-        },
-        {
-            title: <HeadCell>IP Address</HeadCell>,
-            dataIndex: "province",
-            render: (_, entity) => {
-                return <TextCell>{entity.province?.name}</TextCell>;
-            },
-        },
-        {
-            title: <HeadCell>Tổng giao dịch</HeadCell>,
-            dataIndex: "machineType",
-            render: (dom) => {
-                return <TextCell>{dom}</TextCell>;
-            },
-            sorter: (a, b) => {
-                if (a.name && b.name) return a.name.localeCompare(b.name);
-                else return 1;
-            },
-        },
-        {
-            title: <HeadCell>Thành công</HeadCell>,
-            dataIndex: "status",
-            render: (dom) => {
-                return <TextCell>{dom}</TextCell>;
-            },
-            sorter: (a, b) => {
-                if (a.name && b.name) return a.name.localeCompare(b.name);
-                else return 1;
-            },
-        },
-        {
-            title: <HeadCell>Thất bại</HeadCell>,
-            dataIndex: "terminalId",
-            render: (dom) => {
-                return <TextCell>{dom}</TextCell>;
-            },
-            sorter: (a, b) => {
-                if (a.name && b.name) return a.name.localeCompare(b.name);
-                else return 1;
-            },
-        },
-    ];
-    return columns;
+  const columns: ProColumns<TransactionConfiguration>[] = [
+    {
+      title: <HeadCell>Tên máy</HeadCell>,
+      dataIndex: 'machine',
+      render: (_, entity) => {
+        return <TextCell>{entity.machine.name}</TextCell>;
+      },
+      sorter: (a, b) => {
+        if (a.machine.name && b.machine.name) return a.machine.name.localeCompare(b.machine.name);
+        else return 1;
+      },
+    },
+    {
+      title: <HeadCell>Terminal ID</HeadCell>,
+      dataIndex: 'machine',
+      render: (_, entity) => {
+        return <TextCell>{entity.machine.terminalId}</TextCell>;
+      },
+    },
+    {
+      title: <HeadCell>IP Address</HeadCell>,
+      dataIndex: 'machine',
+      render: (_, entity) => {
+        return <TextCell>{entity.machine.ipAddress}</TextCell>;
+      },
+    },
+    {
+      title: <HeadCell>Tổng giao dịch</HeadCell>,
+      dataIndex: 'total',
+      render: (dom) => {
+        return <TextCell>{dom}</TextCell>;
+      },
+      sorter: (a, b) => {
+        return a.total - b.total;
+      },
+    },
+    {
+      title: <HeadCell>Thành công</HeadCell>,
+      dataIndex: 'success',
+      render: (dom) => {
+        return <TextCell>{dom}</TextCell>;
+      },
+      sorter: (a, b) => {
+        return a.success - b.success;
+      },
+    },
+    {
+      title: <HeadCell>Thất bại</HeadCell>,
+      dataIndex: 'failure',
+      render: (dom) => {
+        return <TextCell>{dom}</TextCell>;
+      },
+      sorter: (a, b) => {
+        return a.success - b.success;
+      },
+    },
+  ];
+  return columns;
 }
 
 export default Column;
--- src/pages/SystemWarningTable/index.tsx
+++ src/pages/SystemWarningTable/index.tsx
@@ -1,23 +1,43 @@
-import { addRule, machineList } from "@/services/ant-design-pro/api";
-import type { ActionType, ProColumns } from "@ant-design/pro-components";
+import type { ActionType, ProColumns } from '@ant-design/pro-components';
 // import { getAllUsers } from "@/services/STM-APIs/UserController";
-import {
-    PageContainer,
-    ProFormText,
-    ProFormTextArea,
-    ProTable,
-} from "@ant-design/pro-components";
-import { message } from "antd";
-import { useRef, useState } from "react";
-import { FormattedMessage } from "umi";
+import { PageContainer, ProFormText, ProFormTextArea, ProTable } from '@ant-design/pro-components';
+import { message } from 'antd';
+import { useRef, useState } from 'react';
+import { FormattedMessage } from 'umi';
 // import {useRequest} from "umi";
-import NewUserForm from "./components/forms/NewUserForm";
-import AddNew from "@/components/TableProperties/AddNew";
-import Column from "./components/tables/Column";
+import NewUserForm from './components/forms/NewUserForm';
+import AddNew from '@/components/TableProperties/AddNew';
+import Column from './components/tables/Column';
 // import SelectPage from "./components/tables/SelectPage";
-import style from "@/components/TableProperties/style.less";
-import TitleTable from "@/components/TableProperties/TitleTable";
-import TotalPagination from "@/components/TableProperties/TotalPagination";
+import style from '@/components/TableProperties/style.less';
+import TitleTable from '@/components/TableProperties/TitleTable';
+import TotalPagination from '@/components/TableProperties/TotalPagination';
+
+const genListMachine = (current: number, pageSize: number) => {
+  const tableListDataSource: API.StmInfoResponse[] = [];
+
+  for (let i = 0; i < pageSize; i += 1) {
+    const index = (current - 1) * 10 + i;
+    tableListDataSource.push({
+      id: `${index}`,
+      machineOrder: index * 1000,
+      name: `name-${index}`,
+      location: `location-${index}`,
+      province: {
+        id: 1,
+        name: `province-${index}`,
+      },
+      machineType: 'STM',
+      terminalId: `terminal-${index}`,
+      status: 'OFFLINE',
+      activity: 'MAINTAINING',
+      ipAddress: `ipAddress-${index}`,
+    });
+  }
+  return tableListDataSource;
+};
+
+const listMachine = genListMachine(1, 100);
 
 /**
  * @en-US Add node
@@ -25,184 +45,182 @@ import TotalPagination from "@/components/TableProperties/TotalPagination";
  * @param fields
  */
 const handleAdd = async (fields: API.StmInfoResponse) => {
-    const hide = message.loading("正在添加");
-    try {
-        await addRule({ ...fields });
-        hide();
-        message.success("Added successfully");
-        return true;
-    } catch (error) {
-        hide();
-        message.error("Adding failed, please try again!");
-        return false;
-    }
+  const hide = message.loading('正在添加');
+  try {
+    // await addRule({ ...fields });
+    console.log(fields);
+    hide();
+    message.success('Added successfully');
+    return true;
+  } catch (error) {
+    hide();
+    message.error('Adding failed, please try again!');
+    return false;
+  }
 };
 
 const TableCustom = () => {
-    //--------------- listUSer -----------------------------------
-    // const [listUser, setListUser] = useState<API.StmInfoResponse[] | undefined>();
-    //---------------  handle getAllUser -------------------------------
+  //--------------- listUSer -----------------------------------
+  // const [listUser, setListUser] = useState<API.StmInfoResponse[] | undefined>();
+  //---------------  handle getAllUser -------------------------------
 
-    // const { run: runGetAllUser } = useRequest(
-    //     (params: API.getAllUsersParams) => getAllUsers(params),
-    //     {
-    //         manual: true,
-    //         onSuccess: (res) => {
-    //             const data = res as API.ResponseBasePageResponseObject;
-    //             const listUserRespone = data.data?.items;
-    //             setListUser(listUserRespone);
-    //         },
-    //         onError: (error) => {
-    //             console.log(error);
-    //         },
-    //     }
-    // );
-    /**
-     * @en-US Pop-up window of new window
-     * @zh-CN 新建窗口的弹窗
-     *  */
-    const [createModalVisible, handleModalVisible] = useState<boolean>(false);
-    /**
-     * @en-US The pop-up window of the distribution update window
-     * @zh-CN 分布更新窗口的弹窗
-     * */
-    // const [updateModalVisible, handleUpdateModalVisible] = useState<boolean>(false);
+  // const { run: runGetAllUser } = useRequest(
+  //     (params: API.getAllUsersParams) => getAllUsers(params),
+  //     {
+  //         manual: true,
+  //         onSuccess: (res) => {
+  //             const data = res as API.ResponseBasePageResponseObject;
+  //             const listUserRespone = data.data?.items;
+  //             setListUser(listUserRespone);
+  //         },
+  //         onError: (error) => {
+  //             console.log(error);
+  //         },
+  //     }
+  // );
+  /**
+   * @en-US Pop-up window of new window
+   * @zh-CN 新建窗口的弹窗
+   *  */
+  const [createModalVisible, handleModalVisible] = useState<boolean>(false);
+  /**
+   * @en-US The pop-up window of the distribution update window
+   * @zh-CN 分布更新窗口的弹窗
+   * */
+  // const [updateModalVisible, handleUpdateModalVisible] = useState<boolean>(false);
 
-    const [showDetail, setShowDetail] = useState<boolean>(false);
+  const [showDetail, setShowDetail] = useState<boolean>(false);
 
-    const actionRef = useRef<ActionType>();
-    const [currentRow, setCurrentRow] = useState<API.StmInfoResponse>();
+  const actionRef = useRef<ActionType>();
+  const [currentRow, setCurrentRow] = useState<API.StmInfoResponse>();
 
-    console.log(showDetail, currentRow);
+  console.log(showDetail, currentRow);
 
-    /**
-     * @en-US International configuration
-     * @zh-CN 国际化配置
-     * */
+  /**
+   * @en-US International configuration
+   * @zh-CN 国际化配置
+   * */
 
-    // const [page, setPage] = useState<number>();
-    // const [pageSize, setPageSize] = useState<number>();
-    // const pageSizeRef = useRef<number>(20);
-    const columns: ProColumns<API.StmInfoResponse>[] = Column({
-        setCurrentRow,
-        setShowDetail,
-    });
+  // const [page, setPage] = useState<number>();
+  // const [pageSize, setPageSize] = useState<number>();
+  // const pageSizeRef = useRef<number>(20);
+  const columns: ProColumns<API.StmInfoResponse>[] = Column({
+    setCurrentRow,
+    setShowDetail,
+  });
 
-    const [currentPage, setCurrentPage] = useState<number>(0);
-    const pageSize = useRef<number>(20);
-    // const [totalPage, setTotalPage] = useState<number>(1);
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  const pageSize = useRef<number>(20);
+  // const [totalPage, setTotalPage] = useState<number>(1);
 
-    //-------------- Pagination props --------------------------------
-    const paginationLocale = {
-        items_per_page: "",
-        jump_to: "Trang",
-        page: "",
-    };
+  //-------------- Pagination props --------------------------------
+  const paginationLocale = {
+    items_per_page: '',
+    jump_to: 'Trang',
+    page: '',
+  };
 
-    return (
-        <PageContainer
-            className={style["table-container"]}
-            header={{
-                title: "",
+  return (
+    <PageContainer
+      className={style['table-container']}
+      header={{
+        title: '',
+      }}
+      footer={undefined}
+    >
+      <ProTable
+        headerTitle={<TitleTable>Danh sách máy</TitleTable>}
+        actionRef={actionRef}
+        rowKey="key"
+        search={false}
+        toolBarRender={() => [
+          <AddNew
+            key="primary"
+            onClick={() => {
+              handleModalVisible(true);
             }}
-            footer={undefined}
-        >
-            <ProTable
-                headerTitle={<TitleTable>Danh sách máy</TitleTable>}
-                actionRef={actionRef}
-                rowKey="key"
-                search={false}
-                toolBarRender={() => [
-                    <AddNew
-                        key="primary"
-                        onClick={() => {
-                            handleModalVisible(true);
-                        }}
-                    />,
-                ]}
-                request={machineList}
-                // request={async (params = {}) => {
-                //     const filterParams: API.UserFilter = {
-                //         managementUnit: "",
-                //         staffId: "",
-                //     };
+          />,
+        ]}
+        // request={machineList}
+        dataSource={listMachine}
+        // request={async (params = {}) => {
+        //     const filterParams: API.UserFilter = {
+        //         managementUnit: "",
+        //         staffId: "",
+        //     };
 
-                //     const pageRequestParams: API.PageReq = {
-                //         pageNumber: params.current,
-                //         pageSize: params.pageSize,
-                //         sortDirection: "",
-                //         sortBy: "",
-                //     };
-                //     await runGetAllUser({
-                //         filter: filterParams,
-                //         pageRequest: pageRequestParams,
-                //     });
-                //     return {
-                //         data: listUser,
-                //     };
-                // }}
-                columns={columns}
-                options={false}
-                // rowSelection={{
-                //     onChange: (_, selectedRows) => {
-                //         setSelectedRows(selectedRows);
-                //     },
-                // }}
-                pagination={{
-                    onChange(current) {
-                        setCurrentPage(current);
-                    },
-                    current: currentPage,
-                    className: style["pagination-custom"],
-                    locale: { ...paginationLocale },
-                    showSizeChanger: false,
-                    pageSize: pageSize.current,
-                    showTotal: (total, range) => (
-                        <TotalPagination total={total} range={range} />
-                    ),
-                    hideOnSinglePage: true,
-                    showQuickJumper: true,
-                }}
-            />
+        //     const pageRequestParams: API.PageReq = {
+        //         pageNumber: params.current,
+        //         pageSize: params.pageSize,
+        //         sortDirection: "",
+        //         sortBy: "",
+        //     };
+        //     await runGetAllUser({
+        //         filter: filterParams,
+        //         pageRequest: pageRequestParams,
+        //     });
+        //     return {
+        //         data: listUser,
+        //     };
+        // }}
+        columns={columns}
+        options={false}
+        // rowSelection={{
+        //     onChange: (_, selectedRows) => {
+        //         setSelectedRows(selectedRows);
+        //     },
+        // }}
+        pagination={{
+          onChange(current) {
+            setCurrentPage(current);
+          },
+          current: currentPage,
+          className: style['pagination-custom'],
+          locale: { ...paginationLocale },
+          showSizeChanger: false,
+          pageSize: pageSize.current,
+          showTotal: (total, range) => <TotalPagination total={total} range={range} />,
+          hideOnSinglePage: true,
+          showQuickJumper: true,
+        }}
+      />
 
-            <NewUserForm
-                title="Tạo người dùng mới"
-                width="934px"
-                visible={createModalVisible}
-                onVisibleChange={handleModalVisible}
-                onFinish={async (value) => {
-                    const success = await handleAdd(
-                        value as API.StmInfoResponse
-                    );
-                    if (success) {
-                        handleModalVisible(false);
-                        if (actionRef.current) {
-                            actionRef.current.reload();
-                        }
-                        return true;
-                    }
-                    return false;
-                }}
-            >
-                <ProFormText
-                    rules={[
-                        {
-                            required: true,
-                            message: (
-                                <FormattedMessage
-                                    id="pages.searchTable.ruleName"
-                                    defaultMessage="Rule name is required"
-                                />
-                            ),
-                        },
-                    ]}
-                    width="md"
-                    name="name"
+      <NewUserForm
+        title="Tạo người dùng mới"
+        width="934px"
+        visible={createModalVisible}
+        onVisibleChange={handleModalVisible}
+        onFinish={async (value) => {
+          const success = await handleAdd(value as API.StmInfoResponse);
+          if (success) {
+            handleModalVisible(false);
+            if (actionRef.current) {
+              actionRef.current.reload();
+            }
+            return true;
+          }
+          return false;
+        }}
+      >
+        <ProFormText
+          rules={[
+            {
+              required: true,
+              message: (
+                <FormattedMessage
+                  id="pages.searchTable.ruleName"
+                  defaultMessage="Rule name is required"
                 />
-                <ProFormTextArea width="md" name="desc" />
-            </NewUserForm>
-        </PageContainer>
-    );
+              ),
+            },
+          ]}
+          width="md"
+          name="name"
+        />
+        <ProFormTextArea width="md" name="desc" />
+      </NewUserForm>
+    </PageContainer>
+  );
 };
 
 export default TableCustom;
