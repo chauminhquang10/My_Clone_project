dbd234dbdece90b60a1c73118698e7dd3023a092
viettq <viettq26701@gmail.com>
2022-09-23 16:56:02 +0700

    feat: add validation phone number and email

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -86,3 +86,8 @@ export const ERROR_CODE = {
   700: 'Management unit existed',
   701: "Can't not remove management unit. This unit current includes users or machines",
 };
+
+export const EMAIL_REGEX =
+  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export const PHONE_REGEX = /(84|0[3|5|7|8|9])+([0-9]{8})\b/;
